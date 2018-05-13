f6507fcc06e3aa04b5706907f2d8abb7fc9b66b6
panda <SmelayaPanda@mail.ru>
2018-05-14 02:01:16 +0700

    docs

--- src/components/documents/Documents.vue
+++ src/components/documents/Documents.vue
@@ -4,7 +4,7 @@
     <v-container fluid grid-list-md>
       <v-layout row wrap>
         <v-flex d-flex xs12 sm6 md5>
-          <v-card color="secondary" class="doc_card" dark>
+          <v-card color="secondary" dark>
             <v-card-title primary class="title">Договор на оказание услуг</v-card-title>
             <v-card-text>Типовой договор оказания услуг по созданию сайта предполагает, что Исполнитель
               обязуется
@@ -16,22 +16,23 @@
               Технического Задания к Договору.
             </v-card-text>
             <v-container id="dogovor_wrap">
-              <a href="https://docs.google.com/document/d/1lZ1uJQnpSqgZkeb8YRx73UTJcXatIC1_sto7-LBwTcg/edit?ts=5aa03102" target="_blank">
-                <v-icon class="primary--text">insert_drive_file</v-icon> <span id="dogovor">Документ</span></a>
+              <a href="https://docs.google.com/document/d/1lZ1uJQnpSqgZkeb8YRx73UTJcXatIC1_sto7-LBwTcg/edit?ts=5aa03102"
+                 target="_blank">
+                <v-icon class="primary--text">insert_drive_file</v-icon>
+                <span id="dogovor">Документ</span></a>
             </v-container>
           </v-card>
         </v-flex>
         <v-flex d-flex xs12 sm6 md4>
           <v-layout row wrap>
-            <v-flex d-flex>
+            <v-flex d-flex @click="$bus.$emit('openAdditionalWorksDialog')">
               <v-card color="success" class="doc_card" dark>
-                <v-card-title class="title">Дополнительное соглашение к договору на оказание услуг
-                </v-card-title>
+                <v-card-title class="title">Порядок оказания дополнительных услуг</v-card-title>
               </v-card>
             </v-flex>
             <v-flex d-flex>
-              <v-layout row wrap @click="$bus.$emit('openTechnologyStackDialog')">
-                <v-flex d-flex xs12>
+              <v-layout row wrap>
+                <v-flex d-flex xs12 @click="$bus.$emit('openTechnologyStackDialog')">
                   <v-card color="secondary" class="doc_card" dark>
                     <v-card-title class="title">Стек используемых технологий</v-card-title>
                   </v-card>
@@ -40,7 +41,7 @@
             </v-flex>
           </v-layout>
         </v-flex>
-        <v-flex d-flex xs12 sm6 md3 child-flex>
+        <v-flex d-flex xs12 sm6 md3 child-flex @click="$bus.$emit('openImportantToKnowDialog')">
           <v-card color="success" class="doc_card" dark>
             <v-card-text class="title">Важно знать</v-card-text>
           </v-card>
@@ -59,16 +60,20 @@
     </v-container>
     <!--DIALOGS-->
     <technology-stack/>
+    <additional-works/>
+    <important-to-know/>
   </div>
 </template>
 
 <script>
   import AppRouterName from '../theme/AppRouterName'
   import TechnologyStack from './dialogs/TechnologyStack'
+  import AdditionalWorks from './dialogs/AdditionalWorks'
+  import ImportantToKnow from './dialogs/ImportantToKnow'
 
 export default {
     name: 'Documents',
-    components: {TechnologyStack, AppRouterName},
+    components: {ImportantToKnow, AdditionalWorks, TechnologyStack, AppRouterName},
     data () {
       return {
         msg: {
@@ -86,13 +91,16 @@ export default {
 
   .doc_card:hover {
     cursor: pointer;
-    transform: scale(1.01);
+    transform: scale(1.015);
   }
+
   #dogovor_wrap {
     margin-top: 0;
     padding-top: 0;
   }
+
   #dogovor:hover {
     text-decoration: underline;
   }
+
 </style>
---
+++ src/components/documents/dialogs/AdditionalWorks.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-layout row justify-center>
+    <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+      <v-card>
+        <v-toolbar dark color="primary">
+          <v-btn icon dark @click.native="dialog = false">
+            <v-icon>close</v-icon>
+          </v-btn>
+          <v-toolbar-title>Re:High Studio</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-toolbar-items>
+            <v-btn dark flat @click.native="dialog = false">Ok</v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
+        <v-container>
+          <h3>Дополнительная разработка</h3>
+          <p>
+            Создавать задачи на дополнительную разработку может <b>только студия</b>
+            на основе переговоров с заказчиком или как предложение дальнейшего развития проекта.
+            <b>Только заказчик</b> может сменить статус задачи с "создана" на "ожидает выполнения".
+          </p>
+          <p>
+            Как только задача попадает в статус <b>"ожидает выполнения"</b>
+            и Заказчик производит оплату не менее <b>50% от ее стоимости</b>, студия приступает к выполнению.
+            После завершения задачи Заказчик обязан в течении двух дней принять и оплатить ее полную стоимость.
+          </p>
+          <p>
+            Задачи на дополнительную разработку ставятся с оценочной стоимостью и Исполнитель оставляет за собой право
+            изменить окончательную стоимость по завершении работ, но не более чем на 25% от первоначального значения.
+            В случае неуплаты дальнейшая разработка приостанавливается.
+          </p>
+        </v-container>
+      </v-card>
+    </v-dialog>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    name: 'AdditionalWorks',
+    data () {
+      return {
+        dialog: false
+      }
+    },
+    created () {
+      this.$bus.$on('openAdditionalWorksDialog', () => {
+        this.dialog = true
+      })
+    }
+  }
+</script>
+
+<style scoped>
+</style>
