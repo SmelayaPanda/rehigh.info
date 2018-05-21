65ba07414b17de02a5c152d71b3cf9bcce8e9692
panda <SmelayaPanda@mail.ru>
2018-05-21 23:11:16 +0700

    restore dump

--- src/components/system/System.vue
+++ src/components/system/System.vue
@@ -4,11 +4,28 @@
       <v-icon class="white--text">dns</v-icon>
       Снять копию базы данных
     </el-button>
+    <v-tooltip bottom>
+      <span>
+        Внимание! Это операция полностью перетирает старые данные в базе! <br>
+        Восстановление производить загрузкой файлов <b>.json</b> по одному. <br>
+        <b>Имя файла должно соответствовать названию коллекции!</b>
+      </span>
+    <el-button @click="onPickFile" slot="activator" class="success white--text">
+      <v-icon class="white--text">restore</v-icon>
+        Восстановить коллекцию
+    </el-button>
+    </v-tooltip>
+    <input
+      type="file"
+      style="display: none;"
+      ref="fileInput"
+      accept="json/*"
+      @change="onFilePicked">
   </v-container>
 </template>
 
 <script>
-  import * as firebase from 'firebase'
+  import * as fb from 'firebase'
   import JSZip from 'jszip'
   import FileSaver from 'file-saver'
 
@@ -30,7 +47,7 @@
         this.$store.dispatch('LOADING', true)
 
         let getCollectionData = function (name) {
-          return firebase.firestore().collection(name).get()
+          return fb.firestore().collection(name).get()
             .then(snap => {
               let data = {}
               snap.docs.forEach(el => {
@@ -57,11 +74,40 @@
           .catch(err => {
             console.log(err)
           })
+      },
+      onPickFile () {
+        this.$refs.fileInput.click()
+      },
+      onFilePicked (event) {
+        this.$store.dispatch('LOADING', true)
+        const files = event.target.files
+        let collectionName = files[0].name.split('.')[0]
+        const fileReader = new FileReader()
+        fileReader.addEventListener('load', () => {
+          let actions = []
+          this.imageUrl = fileReader.result
+          console.log(collectionName)
+          let collection = JSON.parse(fileReader.result)
+          for (let docId in collection) {
+            actions.push(fb.firestore().collection(collectionName).doc(docId).set(collection[docId]))
+          }
+          Promise.all(actions).then(() => {
+            console.log('collection: ' + collectionName + ' uploaded')
+            this.$refs.fileInput.value = ''
+            this.$store.dispatch('LOADING', false)
+          })
+            .catch(err => {
+              console.log(err)
+              this.$store.dispatch('LOADING', false)
+            })
+          console.log(actions)
+        })
+        fileReader.readAsText(files[0])
+        this.image = files[0]
       }
     }
   }
 </script>
 
 <style scoped>
-
 </style>
